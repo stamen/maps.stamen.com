3595d3b7cca08244b1eb8d91d2e1a35d8d9716d2
Shawn Allen <shawn@stamen.com>
2012-03-21 11:56:35 -0700

    add (center-synced) home link to embed pages

--- js/embed.js
+++ js/embed.js
@@ -3,7 +3,8 @@
     function init() {
 
         var parent = document.getElementById("map"),
-            provider = getProvider(parent.getAttribute("data-provider"));
+            providerName = parent.getAttribute("data-provider"),
+            provider = getProvider(providerName);
 
         // our main map
         var main = new MM.Map(parent, provider, null,
@@ -13,6 +14,10 @@
         main.setCenterZoom(new MM.Location(37.7706, -122.3782), 12);
 
         setupZoomControls(main);
+
+        syncMapLinks(main, [document.getElementById("home-link")], function(parts) {
+            parts.unshift(providerName);
+        });
 
         var hasher = new MM.Hash(main);
     }
