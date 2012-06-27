7f07f412b97a4ba9b8b1feeb55d01077c090fbae
Shawn Allen <shawn@stamen.com>
2012-06-27 10:52:55 -0700

    added track()

--- js/common.js
+++ js/common.js
@@ -319,3 +319,16 @@ ProviderHash.prototype = {
 };
 
 MM.extend(ProviderHash, MM.Hash);
+
+// Google Analytics tracking
+function track() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32986126-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
