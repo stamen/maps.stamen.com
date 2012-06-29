40f3b4e9fe2790e629adbaef633402ac8ccc0eb3
Shawn Allen <shawn@stamen.com>
2012-06-28 22:51:49 -0700

    don't automatically do it to all the HTML files

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 VERSION=`perl -nle 'print $1 if /tile.stamen.js (v[0-9\\.]+)/' js/tile.stamen.js`
 echo "Updating to version $VERSION..."
-perl -pi -e "s/(tile.stamen.js)([^\"]*)/\$1?$VERSION/g" *.html */*.html
+perl -pi -e "s/(tile.stamen.js)([^\"]*)/\$1?$VERSION/g" $*
