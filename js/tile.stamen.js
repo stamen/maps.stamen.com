c0577f3b95ec68d2998429a7f27fcfb671d6b503
Eric Brelsford <ebrelsford@gmail.com>
2023-09-06 11:28:00 -0400

    Update attribution

--- js/tile.stamen.js
+++ js/tile.stamen.js
@@ -1,22 +1,22 @@
 (function(exports) {
 
 /*
- * tile.stamen.js v1.3.0
+ * tile.stamen.js v1.4.0
  */
 
 var SUBDOMAINS = "a. b. c. d.".split(" "),
     MAKE_PROVIDER = function(layer, type, minZoom, maxZoom) {
         return {
-            "url":          ["//stamen-tiles-{S}a.ssl.fastly.net/", layer, "/{Z}/{X}/{Y}.", type].join(""),
+            "url":          ["//tiles.stadiamaps.com/tiles/stamen_", layer, "/{Z}/{X}/{Y}.", type].join(""),
             "type":         type,
             "subdomains":   SUBDOMAINS.slice(),
             "minZoom":      minZoom,
             "maxZoom":      maxZoom,
-            "attribution":  [
-                'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
-                'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-                'Data by <a href="http://openstreetmap.org/">OpenStreetMap</a>, ',
-                'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+            "attribution": [
+                '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>',
+                '&copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a>',
+                '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>',
+                '&copy; <a href="https://www.openstreetmap.org/about/" target="_blank">OpenStreetMap contributors</a>',
             ].join("")
         };
     },
@@ -24,7 +24,14 @@ var SUBDOMAINS = "a. b. c. d.".split(" "),
         "toner":        MAKE_PROVIDER("toner", "png", 0, 20),
         "terrain":      MAKE_PROVIDER("terrain", "png", 0, 18),
         "terrain-classic": MAKE_PROVIDER("terrain-classic", "png", 0, 18),
-        "watercolor":   MAKE_PROVIDER("watercolor", "jpg", 1, 18),
+        "watercolor": {
+            ...MAKE_PROVIDER("watercolor", "jpg", 1, 18),
+            attribution: [
+                '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>',
+                '&copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a>',
+                '&copy; <a href="https://www.openstreetmap.org/about/" target="_blank">OpenStreetMap contributors</a>',
+            ].join(""),
+        },
         "trees-cabs-crime": {
             "url": "http://{S}.tiles.mapbox.com/v3/stamen.trees-cabs-crime/{Z}/{X}/{Y}.png",
             "type": "png",
@@ -55,32 +62,6 @@ deprecate("toner", ["2010"]);
 
 // toner 2011 flavors
 deprecate("toner", ["2011", "2011-lines", "2011-labels", "2011-lite"]);
-
-var odbl = [
-    "toner",
-    "toner-hybrid",
-    "toner-labels",
-    "toner-lines",
-    "toner-background",
-    "toner-lite",
-    "terrain",
-    "terrain-background",
-    "terrain-lines",
-    "terrain-labels",
-    "terrain-classic"
-];
-
-for (var i = 0; i < odbl.length; i++) {
-    var key = odbl[i];
-
-    PROVIDERS[key].retina = true;
-    PROVIDERS[key].attribution = [
-        'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, ',
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ',
-        'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
-        'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-    ].join("");
-}
 
 /*
  * Export stamen.tile to the provided namespace.
